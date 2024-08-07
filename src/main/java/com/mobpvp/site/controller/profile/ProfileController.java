dad8fcf8d3984e9041d08360400004c9f9134ad2
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2024-08-06 21:23:59 -0400

    fix profile controller using web instead of webResolved

--- src/main/java/com/mobpvp/site/controller/profile/ProfileController.java
+++ src/main/java/com/mobpvp/site/controller/profile/ProfileController.java
@@ -175,7 +175,7 @@ public class ProfileController {
 
         if (!CACHE.contains(uuid)) {
             RequestResponse response = RequestHandler.get(
-                    "profile/%s?web=true&profilePage=true&adminPanel=true",
+                    "profile/%s?webResolved=true&profilePage=true&adminPanel=true",
                     uuid
             );
 
