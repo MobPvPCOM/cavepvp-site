00911bc0880154f2172614774bdbc53f411370a0
curtis <CurtisOK@users.noreply.github.com>
2024-08-21 16:09:59 +0200

    feat: fix caching

--- src/main/java/com/mobpvp/site/config/filter/RateLimitFilter.java
+++ src/main/java/com/mobpvp/site/config/filter/RateLimitFilter.java
@@ -15,7 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 public class RateLimitFilter implements Filter {
 
-    private static final int MAX_REQUESTS_PER_MINUTE = 20;
+    private static final int MAX_REQUESTS_PER_MINUTE = 100;
     private final TimeBasedMap<String, AtomicInteger> requestMap = new TimeBasedMap<>(1, TimeUnit.MINUTES);
 
     @Override
@@ -25,13 +25,18 @@ public class RateLimitFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         String clientIp = request.getRemoteAddr();
 
+        System.out.println("Request from IP: " + clientIp);
         requestMap.putIfAbsent(clientIp, new AtomicInteger(0));
         if (requestMap.get(clientIp).incrementAndGet() > MAX_REQUESTS_PER_MINUTE) {
             response.setStatus(429);
             response.getWriter().write("Rate limit exceeded");
+            System.out.println("Rate limit exceeded for " + clientIp);
             return;
         }
 
+        System.out.println("request : " + request.getRequestURI() + " from " + clientIp);
+
+        System.out.println("Request count for " + clientIp + ": " + requestMap.get(clientIp).get());
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
