0e54dca48b494cd1fb8d9f2a680d00cb46e2d50e
curtis <CurtisOK@users.noreply.github.com>
2024-08-21 15:53:05 +0200

    feat: add rate limit filter

---
+++ src/main/java/com/mobpvp/site/config/filter/RateLimitFilter.java
@@ -0,0 +1,47 @@
+package com.mobpvp.site.config.filter;
+
+import com.mobpvp.site.util.timebased.TimeBasedMap;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@WebFilter(urlPatterns = "/*")
+@Component
+public class RateLimitFilter implements Filter {
+
+    private static final int MAX_REQUESTS_PER_MINUTE = 20;
+    private final TimeBasedMap<String, AtomicInteger> requestMap = new TimeBasedMap<>(1, TimeUnit.MINUTES);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String clientIp = request.getRemoteAddr();
+
+        requestMap.putIfAbsent(clientIp, new AtomicInteger(0));
+        if (requestMap.get(clientIp).incrementAndGet() > MAX_REQUESTS_PER_MINUTE) {
+            response.setStatus(429);
+            response.getWriter().write("Rate limit exceeded");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+}
